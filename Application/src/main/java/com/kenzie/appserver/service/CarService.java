e105c0709a93470359a7d8745aeb00aec6ab60fd
mklein-1997 <YOUREMAIL@YOUREMAIL.com>
2022-12-21 17:47:43 -0500

    Added CarService addCar and removeCar and unit tests

--- Application/src/main/java/com/kenzie/appserver/service/CarService.java
+++ Application/src/main/java/com/kenzie/appserver/service/CarService.java
@@ -14,5 +14,22 @@ public class CarService {
         this.carRepository = carRepository;
     }
 
+    public Car addCar(Car car) {
+        CarRecord record = new CarRecord();
+        record.setMake(car.getMake());
+        record.setModel(car.getModel());
+        record.setYear(car.getYear());
+        record.setTrackingId(car.getTrackingId());
+        record.setAvailable(car.getAvailable());
+        record.setDateRented(car.getDateRented());
+        record.setReturnDate(car.getReturnDate());
+        carRepository.save(record);
+        return car;
+    }
 
+    public Car removeCar(String trackingId) {
+        Car car = carRepository.findByTrackingId(trackingId);
+        carRepository.deleteById(trackingId);
+        return car;
+    }
 }
