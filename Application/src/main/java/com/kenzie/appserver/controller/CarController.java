fb29016ea983cb66a1ceb1d4aa2f6e875eb47c83
DominickD <dominick.dighiera@mykenzie.snhu.edu>
2022-12-27 15:47:32 -0500

    add,remove and get have been modified. added private helper method as well

--- Application/src/main/java/com/kenzie/appserver/controller/CarController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/CarController.java
@@ -5,6 +5,7 @@ import com.kenzie.appserver.controller.model.CarResponse;
 import com.kenzie.appserver.service.CarService;
 
 import com.kenzie.appserver.service.model.Car;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,19 +23,50 @@ public class CarController {
 
 //    @GetMapping("/{trackingId}")
 //    public ResponseEntity<CarResponse> getCarStatus(@PathVariable("trackingId") String trackingId) {
+//        Car car = carService.getCarStatus(trackingId);
 //
+//        if(car == null){
+//            return ResponseEntity.notFound().build();
+//        }
 //
-//        return ResponseEntity.ok().build();
+//        return ResponseEntity.ok().body(carToResponse(car));
 //    }
 
-    //Commented out so the code would compile
 
     @PostMapping
     public ResponseEntity<CarResponse> addCar(@RequestBody CarCreateRequest carCreateRequest) {
+        Car car = new Car(carCreateRequest.getMake(),carCreateRequest.getModel(),carCreateRequest.getYear());
 
+        try{
+            carService.addCar(car);
+        }catch(IllegalArgumentException e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
 
+        return ResponseEntity.ok().body(carToResponse(car));
+    }
+
+    @DeleteMapping("/{trackingId}")
+    public ResponseEntity<CarResponse> removeCar(@PathVariable("trackingId") String trackingId){
+        Car car = carService.removeCar(trackingId);
+
+        if(car == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        return ResponseEntity.ok().body(carToResponse(car));
+    }
+
+    private CarResponse carToResponse(Car car){
         CarResponse carResponse = new CarResponse();
+        carResponse.setTrackingId(car.getTrackingId());
+        carResponse.setMake(car.getMake());
+        carResponse.setModel(car.getModel());
+        carResponse.setYear(car.getYear());
+        carResponse.setAvailable(car.getAvailable());
+        carResponse.setDateRented(car.getDateRented());
+        carResponse.setReturnDate(car.getReturnDate());
 
-        return ResponseEntity.created(URI.create("/example/" + exampleResponse.getId())).body(exampleResponse);
+        return carResponse;
     }
 }
