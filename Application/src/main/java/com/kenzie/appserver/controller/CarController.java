9aa5d4faf98975660ac0071c9edd6d5f0c2a8d6d
DominickDighiera <104596220+DominickDighiera@users.noreply.github.com>
2022-12-28 16:46:20 -0500

    Addcontroller (#7)
    * started controller
    
    * add,remove and get have been modified. added private helper method as well
    
    * Update CarController.java

--- Application/src/main/java/com/kenzie/appserver/controller/CarController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/CarController.java
@@ -4,13 +4,15 @@ import com.kenzie.appserver.controller.model.CarCreateRequest;
 import com.kenzie.appserver.controller.model.CarResponse;
 import com.kenzie.appserver.service.CarService;
 
+import com.kenzie.appserver.service.model.Car;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 @RestController
-@RequestMapping("/example")
+@RequestMapping("/cars")
 public class CarController {
 
     private CarService carService;
@@ -19,22 +21,52 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<CarResponse> get(@PathVariable("id") String id) {
+//    @GetMapping("/{trackingId}")
+//    public ResponseEntity<CarResponse> getCarStatus(@PathVariable("trackingId") String trackingId) {
+//        Car car = carService.getCarStatus(trackingId);
+//
+//        if(car == null){
+//            return ResponseEntity.notFound().build();
+//        }
+//
+//        return ResponseEntity.ok().body(carToResponse(car));
+//    }
 
 
+    @PostMapping
+    public ResponseEntity<CarResponse> addCar(@RequestBody CarCreateRequest carCreateRequest) {
+        Car car = new Car(carCreateRequest.getMake(),carCreateRequest.getModel(),carCreateRequest.getYear());
 
-        return ResponseEntity.ok().build();
+        try{
+            carService.addCar(car);
+        }catch(IllegalArgumentException e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        return ResponseEntity.ok().body(carToResponse(car));
     }
 
-    //Commented out so the code would compile
+    @DeleteMapping("/{trackingId}")
+    public ResponseEntity<CarResponse> removeCar(@PathVariable("trackingId") String trackingId){
+        Car car = carService.removeCar(trackingId);
 
-    //@PostMapping
-    //public ResponseEntity<CarResponse> addNewConcert(@RequestBody CarCreateRequest exampleCreateRequest) {
+        if(car == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
 
+        return ResponseEntity.ok().body(carToResponse(car));
+    }
 
-    //    CarResponse exampleResponse = new CarResponse();
+    private CarResponse carToResponse(Car car){
+        CarResponse carResponse = new CarResponse();
+        carResponse.setTrackingId(car.getTrackingId());
+        carResponse.setMake(car.getMake());
+        carResponse.setModel(car.getModel());
+        carResponse.setYear(car.getYear());
+        carResponse.setAvailable(car.getAvailable());
+        carResponse.setDateRented(car.getDateRented());
+        carResponse.setReturnDate(car.getReturnDate());
 
-    //    return ResponseEntity.created(URI.create("/example/" + exampleResponse.getId())).body(exampleResponse);
-    //}
+        return carResponse;
+    }
 }
